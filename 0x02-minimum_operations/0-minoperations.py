f7286bd0592cf38e138a8e9143163723b3d2023a
OChibu <beze4luv@yahoo.com>
2023-10-10 02:43:31 -0700

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
+'''Coding challenge.
+'''
 
-"""Minimum Operations are those activities necessary for the 
-business or operation to maintain the condition of facilitiy.
-"""
 
 def minOperations(n):
-	"""Returns the fewest number of operations needed 
-	to result in exactly n H characters in the file.
-	"""
-	if not isinstance(n, int) or n < 2:
-        	return 0
-	operation = 0
-	min_factor = 2
-	while n > 1:
-        	while n % min_factor == 0:
-            		operation += min_factor
-            		n //= min_factor
-        	min_factor += 1 
-    
-	return operation
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
